0a45f973497c7eb712305a884d3a60584900066d
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-08-24 14:41:42 -0400

    Before and After mocha functions

--- test/test.js
+++ test/test.js
@@ -8,22 +8,49 @@ const should = chai.should();
 const VtxLedger  = require('../app/ledger')
 
 describe("Ledger JS", function() {
-    const TRUST_ACCOUNT = "vltxtrust00";
+    const DISTRIBUTION_ACCOUNT = "vltxdistrib.";
+    const TRUST_ACCOUNT = "vltxtrust..";
     const TEST_PUBLIC_KEY = "EOS5vBqi8YSzFCeTv4weRTwBzVkGCY5PN5Hm1Gp3133m8g9MtHTbW";
+    const TEST_AMOUNT = 100;
 
-    it("retrieves a balance", function() {
+    let ledger = {}
 
+    before(function() {
         const config = {
             'chainId': process.env.CHAIN_ID,
             'keyProvider': process.env.KEY_PROVIDER,
             'httpEndpoint': process.env.HTTP_ENDPOINT
         };
 
-        const ledger = new VtxLedger(config);
-        
+        ledger = new VtxLedger(config);
+
+        return ledger.recordTransfer({
+            from: {
+                account: DISTRIBUTION_ACCOUNT
+            }, to: {
+                account: TRUST_ACCOUNT,
+                key: TEST_PUBLIC_KEY
+            },
+            amount: TEST_AMOUNT
+        })
+    })
+
+    after(function() {
+        return ledger.recordTransfer({
+            from: {
+                account: TRUST_ACCOUNT,
+                key: TEST_PUBLIC_KEY
+            }, to: {
+                account: DISTRIBUTION_ACCOUNT
+            },
+            amount: TEST_AMOUNT
+        })
+    })
+
+    it("retrieves a balance", function() {
         return ledger.retrieveBalance({
           account: TRUST_ACCOUNT,
           key: TEST_PUBLIC_KEY
-        }).should.eventually.equal(100)
+        }).should.eventually.equal(TEST_AMOUNT)
      });
 });
