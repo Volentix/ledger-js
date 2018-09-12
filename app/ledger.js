cf0739e787e3f14968d5a1b3adb752ea9f51ef28
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-09-11 20:53:07 -0400

    Use async / await

--- app/ledger.js
+++ app/ledger.js
@@ -1,5 +1,7 @@
 Eos = require("eosjs");
 
+const moment = require("moment");
+
 class Ledger {
   constructor(config) {
     this.LEDGER_ACCOUNT_NAME = "vtxledger";
@@ -19,14 +21,14 @@ class Ledger {
   async retrieveBalance({ account, key }) {
     const contract = await this.eos.contract(this.LEDGER_ACCOUNT_NAME);
 
-    return contract
-      .getblnc({
-        account,
-        tokey: key ? key : ""
-      })
-      .then(output =>
-        JSON.parse(output.processed.action_traces[0].console.replace(/'/g, '"'))
-      );
+    const balance = await contract.getblnc({
+      account,
+      tokey: key ? key : ""
+    });
+
+    return JSON.parse(
+      balance.processed.action_traces[0].console.replace(/'/g, '"')
+    );
   }
 
   // {
@@ -48,24 +50,25 @@ class Ledger {
   async recordTransfer({ from, to, amount }) {
     const contract = await this.eos.contract(this.LEDGER_ACCOUNT_NAME);
 
-    return contract
-      .rcrdtfr({
-        s: this.TREASURY_ACCOUNT_NAME,
-        fromaccount: from.account,
-        toaccount: to.account,
-        fromkey: from.key ? from.key : "",
-        tokey: to.key ? to.key : "",
-        amount
-      })
-      .then(transfer => {
-        return {
-          from,
-          to,
-          amount,
-          id: transfer.processed.id,
-          currency: "VTX" // TODO Should be returned from server
-        };
-      });
+    const submittedAt = new Date();
+
+    const transfer = await contract.rcrdtfr({
+      s: this.TREASURY_ACCOUNT_NAME,
+      fromaccount: from.account,
+      toaccount: to.account,
+      fromkey: from.key ? from.key : "",
+      tokey: to.key ? to.key : "",
+      amount
+    });
+
+    return {
+      from,
+      to,
+      amount,
+      submittedAt: moment(submittedAt).format(),
+      id: transfer.processed.id,
+      currency: "VTX" // TODO Should be returned from server
+    };
   }
 
   // Retrieve all transactions performed from / to this account & key
